3d61385629231d0213c0ffea64f6f43d70eda9e2
Cory Walker <cory.walker.dev@gmail.com>
2023-05-26 20:17:18 -0700

    make header with responsive nav bar

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+var hamburger = document.querySelector('.hamburger');
+var nav = document.querySelector('nav');
+var navlinks = Array.from(document.getElementsByClassName('navlink'));
+
+hamburger.onclick = toggleNav;
+
+function toggleNav() {
+  toggleNavScale();
+  toggleNavLinksOpacity();
+}
+
+function toggleNavScale() {
+  if (
+    window.getComputedStyle(nav).transform ==
+    'matrix(1, 0, 0, 0, 0, 0)'
+  ) {
+    nav.style.transform = 'scaleY(1)';
+  } else {
+    nav.style.transform = 'scaleY(0)';
+  }
+}
+
+function toggleNavLinksOpacity() {
+  if (window.getComputedStyle(navlinks[1]).opacity == '0') {
+    navlinks.forEach((navlink) => (navlink.style.opacity = '1'));
+  } else {
+    navlinks.forEach((navlink) => (navlink.style.opacity = '0'));
+  }
+}
